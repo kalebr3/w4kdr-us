aa4a9ffe741a119671526b55fedc43bc487853a0
Kaleb Redman <kaleb@redman.us>
2021-06-08 17:04:56 -0400

    change nasa api key env var

--- pages/api/nasa/apod.js
+++ pages/api/nasa/apod.js
@@ -6,7 +6,7 @@ const nasa = axios.create({
 
 export default async (_, res) => {
   const response = await nasa.get("/planetary/apod/", {
-    params: { api_key: process.env.NEXT_PUBLIC_NASA_API_KEY },
+    params: { api_key: process.env.NASA_API_KEY },
   });
   const data = response.data;
   res.status(200).json({ ...data });
