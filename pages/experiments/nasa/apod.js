d940f3b51c3323e753104155ab74bc124f548779
Kaleb Redman <kaleb@redman.us>
2021-06-08 16:35:59 -0400

    refactor to use swr with new api endpoint

--- pages/experiments/nasa/apod.js
+++ pages/experiments/nasa/apod.js
@@ -1,23 +1,15 @@
-import { useState, useEffect } from "react";
-
 import Layout from "components/common/layout";
-import APODCard from "components/apodCard";
 
 import { getPostBySlug } from "services/LocalMarkdown";
 import md2html from "utils/md2html";
 
-import { getAPOD } from "services/NASA";
+import { useAPOD } from "services/swrHooks";
 
 export default function APOD({ post }) {
-  const [apod, setAPOD] = useState({});
+  const { apod, isLoading, isError } = useAPOD();
 
-  useEffect(() => {
-    const fetchData = async () => {
-      const response = await getAPOD();
-      setAPOD(response.data);
-    };
-    fetchData();
-  }, []);
+  if (isError) return <div>ERROR!</div>;
+  if (isLoading) return <div>LOADING!</div>;
 
   return (
     <Layout heading="NASA Astronomy Picture of the Day">
@@ -28,7 +20,17 @@ export default function APOD({ post }) {
         />
       </div>
       <div className="bg-gray-200">
-        <APODCard apod={apod} />
+        <div className="m-2 p-4 grid gap-6 grid-col-1 lg:grid-cols-12 bg-gray-200">
+          <div className="lg:col-span-5">
+            <img src={apod.url} alt={apod.title} />
+          </div>
+          <div className="lg:col-span-7">
+            <h1 className="text-3xl">{apod.title}</h1>
+            <h2 className="text-2xl">{apod.copyright}</h2>
+            <p>{apod.date}</p>
+            <p className="text-lg">{apod.explanation}</p>
+          </div>
+        </div>
       </div>
     </Layout>
   );
